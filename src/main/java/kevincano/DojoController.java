144353c1d9414d3773fd18b1c6ed6fe242d57385
kcano23 <kacano@fullsail.edu>
2021-04-12 21:15:15 -0400

    first commit

---
+++ src/main/java/kevincano/DojoController.java
@@ -0,0 +1,24 @@
+package kevincano;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DojoController {
+	@RequestMapping("/{location}")
+    public String showLocation(@PathVariable("location") String location) {
+		if(location.equals("dojo")) {
+			return "The dojo is awesome!";
+		}else if(location.equals("burbank-dojo")) {
+			return "Burbank Dojo is located in Southern California";
+		}
+		else if(location.equals("san-jose")) {
+			return "SJ dojo is the headquarters.";
+		}
+		else {
+			return "Sorry this Dojo is not available.";
+		}
+    }
+
+}
---
+++ src/main/java/kevincano/StringsApplication.java
@@ -0,0 +1,26 @@
+package kevincano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+
+//@RestController
+public class StringsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringsApplication.class, args);
+	}
+//	// 1. Annotation
+//    @RequestMapping("/")
+//    // 3. Method that maps to the request route above
+//    public String hello() {
+//            return "Hello client! How are you doing?";
+//    }
+//    @RequestMapping("/random")
+//    public String random() {
+//            return "Spring Boot is great! So easy to just respond with strings";
+//    }
+}
